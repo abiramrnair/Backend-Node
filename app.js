28c97cbfafcb92b6c4e4180f6b78b7f502e40123
Abiram Nair <abiramrnair@gmail.com>
2020-11-15 12:10:23 -0500

    headers

--- app.js
+++ app.js
@@ -15,10 +15,11 @@ app.use(express.static('Public'));
 app.use(cors())
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Colontrol-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token, x-refresh-token, _id");
-
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
     res.header(
         'Access-Control-Expose-Headers',
         'x-access-token, x-refresh-token'
