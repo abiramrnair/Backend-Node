f1630456ab0b8e74f080b1d08c9b9604a240b179
Abiram Nair <abiramrnair@gmail.com>
2020-11-15 12:20:05 -0500

    access origin

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.static('Public'));
 app.use(cors())
 
 app.use(function (req, res, next) {
-    res.header("Access-Colontrol-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token, x-refresh-token, _id");
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
